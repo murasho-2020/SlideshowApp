b6566bb9a45b827325369575e202e5da94d2d14a
murasho <gyudon2929@gmail.com>
2020-07-15 00:56:35 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Shousei  Murakami on 2020/07/15.
+//  Copyright © 2020 shousei.murakami2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

a029e829fbc177aed2a006dd428516599240a767
murasho <gyudon2929@gmail.com>
2020-07-24 21:35:25 +0900

    1. IBAction の onTapAction メソッドのカッコ内にて、コードとコメントを修正しました。
    2. ⌘ + aで全選択して control + i でインデント（段付）を修正しました。
    
    3. IBAction の switchButtonTap メソッドの1文字「s」だけが抜け落ちていたため、不具合の原因になっていたかもしれません。switchButtonTapと正しく表記しました。
    
    4.コミットに際して、xcdebuggerという ？マーク付きの謎フォルダがどうしても消えないので、一緒にコミットします。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var SlideImage: UIImageView!
     
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var switchButton: UIButton!
     
-   
+    
     // タイマーを格納するプロパティ変数timerを宣言
     var timer: Timer!
-
+    
     // タイマー用の時間のための変数
     var timer_sec: Float = 0
     
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
     
     // 画像の名前の配列
     let imageNameArray = ["IMG_8765.png", "IMG_8766.png", "IMG_8767.png", "IMG_8768.png", "IMG_8769.png"]
-
+    
     // 表示している画像の番号を元に画像を表示する
     func displayImage() {
         // 表示している画像の番号から名前を取り出し
@@ -41,25 +41,25 @@ class ViewController: UIViewController {
         SlideImage.image = image
     }
     
-     override func viewDidLoad() {
-         super.viewDidLoad()
-    // 初期画面
-     }
-
-     // selector: #selector(displayImage()) で指定された関数
-     // timeInterval: 2.0, repeats: true で指定された通り、2.0秒毎に呼び出され続ける
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 初期画面
+    }
+    
+    // selector: #selector(displayImage()) で指定された関数
+    // timeInterval: 2.0, repeats: true で指定された通り、2.0秒毎に呼び出され続ける
     @objc func updateTimer(_ timer: Timer){
         // 進むボタンの内容を行う
         if displayImageNo < imageNameArray.count - 1 {
-                  // 表示している画像の番号を1増やす
+            // 表示している画像の番号を1増やす
             displayImageNo += 1
-                  // 表示している画像の番号を元に画像を表示する
+            // 表示している画像の番号を元に画像を表示する
             displayImage()
-              } else {
-                  displayImageNo = 0
-                  displayImage()
-              }
-     }
+        } else {
+            displayImageNo = 0
+            displayImage()
+        }
+    }
     
     // 進むボタン IBAction
     @IBAction func nextButtonTap(_ sender: Any) {
@@ -88,72 +88,84 @@ class ViewController: UIViewController {
     }
     
     // 再生/停止ボタン IBAction
-    @IBAction func witchButtonTap(_ sender: Any) {
-        // 動作中のタイマーを1つに保つために、 timer が存在しない場合だけ、タイマーを生成して動作させる
-                if self.timer == nil {
-                    // タイマーの作成、始動
-                    self.timer = Timer.scheduledTimer(timeInterval: 2.0,target: self,
-                                    selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
-                    
-                    // 再生・停止ボタンタップ時に進むボタン・戻るボタンのタップ非表示
-                    nextButton.isEnabled = false
-                    backButton.isEnabled = false
-                    
-                    // ボタンの名前を停止とする
-                    switchButton.setTitle("停止", for: .normal)
-                    
-                    // 背景色・ボタン内テキストサイズを変更
-                    self.view.backgroundColor = UIColor.lightGray
-                    switchButton.titleLabel?.font =
-                    UIFont.systemFont(ofSize: 25)
-                    
-                } else if self.timer != nil {
-                    // タイマーを停止
-                    self.timer.invalidate()
-                    
-                    // nil にして再び再生(nil の時にタイマー生成)
-                    self.timer = nil
-                    
-                    // 再生・停止ボタンタップ時に進むボタン・戻るボタンのタップ非表示
-                    nextButton.isEnabled = true
-                    backButton.isEnabled = true
-                    
-                    // ボタンの名前を再生とする
-                    switchButton.setTitle("再生", for: .normal)
-                    
-                    // 背景色・ボタン内テキストサイズを戻す
-                    self.view.backgroundColor = UIColor.white
-                    switchButton.titleLabel?.font =
-                    UIFont.systemFont(ofSize: 20)
-                }
-    }
-   
-   // 画像がタップされたとき処理が行えるように、Tap Gesture Recognizerを接続
-    @IBAction func onTapAction(_ sender: Any) {
-    
-         self.performSegue(withIdentifier: "toZoomIn", sender: nil)
+    @IBAction func switchButtonTap(_ sender: Any) {
         
-       if self.timer != nil {
+        // 動作中のタイマーを1つに保つために、 timer が存在しない場合だけ、タイマーを生成して動作させる
+        if self.timer == nil {
+            // タイマーの作成、始動
+            self.timer = Timer.scheduledTimer(timeInterval: 2.0,target: self,
+                                              selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+            
+            // 再生・停止ボタンタップ時に進むボタン・戻るボタンの非表示
+            nextButton.isEnabled = false
+            backButton.isEnabled = false
+            
+            // ボタンの名前を停止とする
+            switchButton.setTitle("停止", for: .normal)
+            
+            // 背景色・ボタン内テキストサイズを変更
+            self.view.backgroundColor = UIColor.lightGray
+            switchButton.titleLabel?.font =
+                UIFont.systemFont(ofSize: 25)
+            
+        } else if self.timer != nil {
+            // タイマーを停止
+            self.timer.invalidate()
+            
+            // nil にして再び再生(nil の時にタイマー生成)
+            self.timer = nil
+            
+            // 再生・停止ボタンタップ時に進むボタン・戻るボタンの表示
+            nextButton.isEnabled = true
+            backButton.isEnabled = true
+            
+            // ボタンの名前を再生とする
+            switchButton.setTitle("再生", for: .normal)
+            
+            // 背景色・ボタン内テキストサイズを戻す
+            self.view.backgroundColor = UIColor.white
+            switchButton.titleLabel?.font =
+                UIFont.systemFont(ofSize: 20)
+        }
+    }
+    
+    // 画像がタップされたとき処理が行えるように、Tap Gesture Recognizerを接続
+    @IBAction func onTapAction(_ sender: Any) {
+        
+        self.performSegue(withIdentifier: "toZoomIn", sender: nil)
+        
+        if self.timer != nil {
             // タイマーを停止する
-        self.timer.invalidate()
-                          
-        // nil にして再び再生(nil の時にタイマー生成)
-        self.timer = nil
+            self.timer.invalidate()
+            
+            // nil にして再び再生(nil の時にタイマー生成)
+            self.timer = nil
+            
+            // 再生・停止ボタンタップ時に進むボタン・戻るボタンの表示
+            nextButton.isEnabled = true
+            backButton.isEnabled = true
+            
+            // ボタンの名前を再生とする
+            switchButton.setTitle("再生", for: .normal)
+            
+            // 背景色・ボタン内テキストサイズを戻す
+            self.view.backgroundColor = UIColor.white
+            switchButton.titleLabel?.font =
+                UIFont.systemFont(ofSize: 20)
         }
     }
     
     // 遷移元から遷移先にデータ(画像)を渡す
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            // segueから遷移先のResultViewControllerのインスタンスを取得する
-            let ZoomInViewController:ZoomInViewController = segue.destination as! ZoomInViewController
-            // 表示している画像の番号から名前を取り出し
-            let name = imageNameArray[displayImageNo]
-            // 画像を読み込み
-            let image = UIImage(named: name)
-            
-            // 遷移先のZoomInViewControllerで宣言しているselectedImgに値を代入して渡す
-            ZoomInViewController.selectedImg = image
-        }
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerのインスタンスを取得する
+        let ZoomInViewController:ZoomInViewController = segue.destination as! ZoomInViewController
+        // 表示している画像の番号から名前を取り出し
+        let name = imageNameArray[displayImageNo]
+        // 画像を読み込み
+        let image = UIImage(named: name)
+        
+        // 遷移先のZoomInViewControllerで宣言しているselectedImgに値を代入して渡す
+        ZoomInViewController.selectedImg = image
+    }
 }
 
